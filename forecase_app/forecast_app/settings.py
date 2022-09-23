fdf463c073a19ec7e2acba835a6c2921162ed47e
Vanna Phan <phansovanna18@gmail.com>
2022-09-23 22:22:18 +0700

    update database

--- forecase_app/forecast_app/settings.py
+++ forecase_app/forecast_app/settings.py
@@ -90,18 +90,18 @@ WSGI_APPLICATION = 'forecast_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'forecast',
-        'USER': 'forecast_user',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'forecast',
+    #     'USER': 'forecast_user',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
 }
 
 
